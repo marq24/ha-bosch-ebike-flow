c97fb9564119040f698a97bc12cc7f070670e2a3
marq24 <marq24@emac.de>
2026-03-13 09:27:10 +0100

    added GPSLogger fields

--- custom_components/bosch_ebike_ble/const.py
+++ custom_components/bosch_ebike_ble/const.py
@@ -3,7 +3,6 @@
 DOMAIN = "bosch_ebike_ble"
 
 # Bosch eBike BLE Service UUIDs
-# From: https://github.com/RobbyPee/Bosch-Smart-System-Ebike-Garmin-Android
 BOSCH_STATUS_SERVICE_UUID = "00000010-eaa2-11e9-81b4-2a2ae2dbcce4"
 BOSCH_STATUS_CHAR_UUID = "00000011-eaa2-11e9-81b4-2a2ae2dbcce4"
 
